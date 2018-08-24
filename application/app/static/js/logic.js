ca809868aa09016085542daca84011d332f74cd0
Gryazzz <33532469+Gryazzz@users.noreply.github.com>
2018-08-23 20:51:49 -0400

    interface change

--- application/app/static/js/logic.js
+++ application/app/static/js/logic.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
     console.log("I'm ready")
+    
 });
 
 $("#submitAuth").click(function(event) {
@@ -15,4 +16,14 @@ $("#submitGmail").click(function(event) {
 $("#submitUpload").click(function(event) {
     event.preventDefault();
     $("#uploadForm").submit();		
+});
+
+$('.password-group').children(".fa").click(function() {
+    $(this).toggleClass("fa-eye fa-eye-slash");
+    var type = $('.pass').attr("type");
+    if(type === 'password'){
+        $('.pass').attr("type", "text");
+    } else {
+        $('.pass').attr("type", "password");
+    }
 });
